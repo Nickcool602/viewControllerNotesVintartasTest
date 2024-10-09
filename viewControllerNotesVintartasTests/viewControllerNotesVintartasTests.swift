c8327dfcc70f2cd947e4ea9dd83eb042e31aeed9
Nick VIntartas <nv2009123@gmail.com>
2024-10-09 14:38:00 -0500

    Initial Commit

---
+++ viewControllerNotesVintartasTests/viewControllerNotesVintartasTests.swift
@@ -0,0 +1,17 @@
+//
+//  viewControllerNotesVintartasTests.swift
+//  viewControllerNotesVintartasTests
+//
+//  Created by NICHOLAS VINTARTAS on 10/9/24.
+//
+
+import Testing
+@testable import viewControllerNotesVintartas
+
+struct viewControllerNotesVintartasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
